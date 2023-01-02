6812bcad47789d331fc67ff9eaf7e60ec3d17436
Norberto Gomez <norgomez1@gmail.com>
2023-01-02 08:56:57 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link  } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Error from './pages/Error';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <h1> This is the Nav /</h1>
+      </nav>
+
+      <Link to="/"> Home</Link>
+      <Link to="/about"> About</Link>
+      <Link to="/profile"> Profile</Link>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+
+      <footer>
+        <h2>This is the footer /</h2>
+      </footer>
+    </Router>
   );
 }
 
---
+++ src/pages/Error.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <div>Error page not FOUND</div>
+  )
+}
+
+export default Error
